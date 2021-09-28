463b9dbac7ccbd804ddd86d29de16ac7945f82c7
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2021-09-28 14:03:53 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,70 @@
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    gamestart = 1
+    player = game.createSprite(2, 4)
+    astroid = game.createSprite(randint(0, 4), 0)
+    astroid.turn(Direction.Right, 90)
+    astroid.set(LedSpriteProperty.Brightness, 50)
+})
+input.onButtonPressed(Button.A, function () {
+    player.change(LedSpriteProperty.X, -1)
+})
+function move_astroid () {
+    basic.pause(speed)
+    astroid.move(1)
+}
+function lives2 () {
+    if (lives == 0) {
+        basic.showIcon(IconNames.Skull)
+        game.gameOver()
+    } else {
+        basic.showNumber(lives)
+        basic.showIcon(IconNames.Heart)
+        basic.pause(100)
+    }
+}
+function setastriod () {
+    astroid.set(LedSpriteProperty.X, randint(0, 4))
+    astroid.set(LedSpriteProperty.Y, 0)
+}
+input.onButtonPressed(Button.B, function () {
+    player.change(LedSpriteProperty.X, 1)
+})
+let astroid: game.LedSprite = null
+let player: game.LedSprite = null
+let gamestart = 0
+let lives = 0
+let speed = 0
+speed = 550
+lives = 3
+gamestart = 0
+lives2()
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # . # . #
+    . . # . .
+    . . # . .
+    `)
+basic.forever(function () {
+    if (gamestart == 1) {
+        if (speed == 29) {
+            speed = 30
+        }
+        if (astroid.isTouching(player)) {
+            lives += -1
+            lives2()
+            setastriod()
+        } else if (astroid.get(LedSpriteProperty.Y) == 4) {
+            setastriod()
+        } else {
+            move_astroid()
+        }
+        if (speed > 300) {
+            speed += -10
+        } else if (speed > 150) {
+            speed += -5
+        } else if (speed > 75) {
+            speed += -1
+        }
+    }
+})
